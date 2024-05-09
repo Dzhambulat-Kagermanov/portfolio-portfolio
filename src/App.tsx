e19bcb09bca30ed1d9552e94c32707a25658d035
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-05-09 11:21:38 +0300

    ADD: Add the Head bloc in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import '@/assets/css/imports.css'
+import Head from './components/modules/Head/Head'
 import Header from './components/modules/Header/Header'
 
 function App() {
   return (
     <>
       <Header />
+      <Head />
     </>
   )
 }

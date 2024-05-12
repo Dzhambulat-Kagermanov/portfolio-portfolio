3d5980a48211d0dbadb945ca877744abe9f75e3c
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-05-12 11:27:19 +0300

    FIX: Fix in styles for Skills module in src\components\modules\Skills

--- src/components/modules/Skills/Skills.tsx
+++ src/components/modules/Skills/Skills.tsx
@@ -2,10 +2,12 @@ import BaseStructure from '@/components/elements/BaseStructure/BaseStructure'
 import { FC } from 'react'
 import Content from './Content/Content'
 import Decoration from './Decoration/Decoration'
+import './Skills.scss'
+import './Adaptive.scss'
 
 const Skills: FC = () => {
   return (
-    <BaseStructure containerClass='--parentBlock'>
+    <BaseStructure containerClass='--parentBlock' innerClass='skills-block'>
       <Content className='skills-block__content' />
       <Decoration className='skills-block__decoration' />
     </BaseStructure>

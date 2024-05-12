a6fa1ded86dc56e1e291e231bb802f1307338339
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-05-12 10:50:51 +0300

    CREATE: Create download file resume in download and add image for skills module. Skills module was add in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import '@/assets/css/imports.css'
 import Head from './components/modules/Head/Head'
 import Header from './components/modules/Header/Header'
+import Skills from './components/modules/Skills/Skills'
 
 function App() {
   return (
     <>
       <Header />
       <Head />
+      <Skills />
     </>
   )
 }

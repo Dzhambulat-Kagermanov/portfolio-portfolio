b03a77d2dca87c4e1d75f4b4dbd2764db61156af
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-05-15 14:16:27 +0300

    FIX: Fixed an issue with copying text in the Contacts module. The wrong text was copied. src/components/modules/Contacts

--- src/components/modules/Contacts/Content/Group/Item/Item.tsx
+++ src/components/modules/Contacts/Content/Group/Item/Item.tsx
@@ -3,10 +3,14 @@ import { FC } from 'react'
 import classes from './Item.module.scss'
 import { contactsDataType } from '@/constants/contacts'
 import CopyButton from '@/components/ui/button/CopyButton'
+import { useAppDispatch } from '@/hooks/reduxHooks'
+import { setHintMessage } from '@/store/hint/hintSlice'
+// import useHint from '@/hooks/useHint'
 
 interface IItemProps extends IClassName, contactsDataType {}
 
 const Item: FC<IItemProps> = ({ messenger, nickname, className }) => {
+  const dispatch = useAppDispatch()
   return (
     <li className={`${classes.item} ${className || ''}`}>
       <div className={`${classes.field} contacts-group-item__field`}>
@@ -15,7 +19,14 @@ const Item: FC<IItemProps> = ({ messenger, nickname, className }) => {
         </h2>
         <CopyButton
           className='contacts-group-item-field__btn'
-          onClick={() => {}}
+          onClick={() => {
+            dispatch({
+              type: setHintMessage.type,
+              payload: { message: 'Текст успешно скопирован' },
+            })
+            // useHint(500)
+            navigator.clipboard.writeText(nickname)
+          }}
         />
       </div>
       <h3 className='comp-text-q-r-3'>{messenger}</h3>
